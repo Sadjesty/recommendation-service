8557e478389b31b9fb61cf78ebcc3168062f51e4
sadjesty <sadjesty@gmail.com>
2022-05-30 17:27:18 +0500

    added method that return the crypto with the highest normalized range for a specific day

--- src/main/java/com/xm/recommendationservice/controller/PricesController.java
+++ src/main/java/com/xm/recommendationservice/controller/PricesController.java
@@ -4,12 +4,11 @@ import com.xm.recommendationservice.domain.CryptoNormalizedRange;
 import com.xm.recommendationservice.domain.CryptoPrice;
 import com.xm.recommendationservice.service.PriceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
@@ -22,17 +21,17 @@ public class PricesController {
 
     @GetMapping("/{name}/allMonthPrices")
     public List<CryptoPrice> getAllMonthPrices(@PathVariable String name) throws IOException {
-        return priceService.getAllPricesForMonth(name);
+        return priceService.getAllPrices(name);
     }
 
     @GetMapping("/{name}/minMonthPrice")
     public CryptoPrice getMinMonthPrice(@PathVariable String name) throws IOException {
-        return priceService.getMinMonthPrice(name);
+        return priceService.getMinPrice(name);
     }
 
     @GetMapping("/{name}/maxMonthPrice")
     public CryptoPrice getMaxMonthPrice(@PathVariable String name) throws IOException {
-        return priceService.getMaxMonthPrice(name);
+        return priceService.getMaxPrice(name);
     }
 
     @GetMapping("/{name}/oldestMonthPrice")
@@ -48,5 +47,12 @@ public class PricesController {
     @GetMapping("/allCryptos")
     public Set<CryptoNormalizedRange> getAllCryptos() throws IOException {
         return priceService.getAllCryptos();
+    }
+
+    @GetMapping("/bestCrypto")
+    public CryptoNormalizedRange getBestCrypto(@RequestParam(value = "date")
+                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                               LocalDate date) throws IOException {
+        return priceService.getCryptoWithHighestNormalizedRangeByDate(date);
     }
 }
--- src/main/java/com/xm/recommendationservice/service/PriceService.java
+++ src/main/java/com/xm/recommendationservice/service/PriceService.java
@@ -12,10 +12,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.time.LocalDate;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,27 +23,27 @@ public class PriceService {
     private static final String FILE_SUFFIX = "_values.csv";
     public static final String FILE_NAME_REGEXP = "..._values\\.csv";
 
-    public List<CryptoPrice> getAllPricesForMonth(String symbol) throws IOException {
+    public List<CryptoPrice> getAllPrices(String symbol) throws IOException {
         File file = getFileFromSymbol(symbol);
-        return getAllPricesForMonth(file);
+        return getAllPrices(file);
     }
 
-    public CryptoPrice getMinMonthPrice(String symbol) throws IOException {
+    public CryptoPrice getMinPrice(String symbol) throws IOException {
         File file = getFileFromSymbol(symbol);
-        return getMinMonthPrice(file);
+        return getMinPrice(file);
     }
 
-    public CryptoPrice getMaxMonthPrice(String symbol) throws IOException {
+    public CryptoPrice getMaxPrice(String symbol) throws IOException {
         File file = getFileFromSymbol(symbol);
-        return getMaxMonthPrice(file);
+        return getMaxPrice(file);
     }
 
     public CryptoPrice getOldestMonthPrice(String symbol) throws IOException {
-        return getAllPricesForMonth(symbol).stream().min(Comparator.comparing(CryptoPrice::getPriceTime)).orElseThrow();
+        return getAllPrices(symbol).stream().min(Comparator.comparing(CryptoPrice::getPriceTime)).orElseThrow();
     }
 
     public CryptoPrice getNewestMonthPrice(String symbol) throws IOException {
-        return getAllPricesForMonth(symbol).stream().max(Comparator.comparing(CryptoPrice::getPriceTime)).orElseThrow();
+        return getAllPrices(symbol).stream().max(Comparator.comparing(CryptoPrice::getPriceTime)).orElseThrow();
     }
 
     public Set<CryptoNormalizedRange> getAllCryptos() throws IOException {
@@ -58,30 +56,57 @@ public class PriceService {
         }
     }
 
+    public CryptoNormalizedRange getCryptoWithHighestNormalizedRangeByDate(LocalDate date) throws IOException {
+        File directory = new ClassPathResource(FOLDER_NAME).getFile();
+        File[] files = getAllSuitableFiles(directory);
+        if (files != null) {
+            return getCryptoWithHighestNormalizedRangeByDate(files, date);
+        } else {
+            return null;
+        }
+    }
+
     private Set<CryptoNormalizedRange> getCryptoNormalizedRangesSetInDescendingOrder(File[] files) throws IOException {
         Comparator<CryptoNormalizedRange> comparator =
                 Comparator.comparing(CryptoNormalizedRange::getNormalizedRange).reversed();
         Set<CryptoNormalizedRange> allCryptos = new TreeSet<>(comparator);
         for (File file : files) {
-            CryptoNormalizedRange cryptoNormalizedRange = getCryptoNormalizedRange(file);
+            CryptoPrice maxCryptoPrice = getMaxPrice(file);
+            CryptoPrice minCryptoPrice = getMinPrice(file);
+            CryptoNormalizedRange cryptoNormalizedRange = getCryptoNormalizedRange(maxCryptoPrice.getPriceValue(),
+                    minCryptoPrice.getPriceValue(), maxCryptoPrice.getCurrencySymbol());
             allCryptos.add(cryptoNormalizedRange);
         }
         return allCryptos;
     }
 
-    private CryptoNormalizedRange getCryptoNormalizedRange(File file) throws IOException {
-        CryptoPrice maxMonthCryptoPrice = getMaxMonthPrice(file);
-        CryptoPrice minMonthCryptoPrice = getMinMonthPrice(file);
-        BigDecimal maxMonthPrice = maxMonthCryptoPrice.getPriceValue();
-        BigDecimal minMonthPrice = minMonthCryptoPrice.getPriceValue();
+    private CryptoNormalizedRange getCryptoWithHighestNormalizedRangeByDate(File[] files, LocalDate date)
+            throws IOException {
+        List<CryptoNormalizedRange> allCryptos = new ArrayList<>();
+        for (File file : files) {
+            try {
+                CryptoPrice maxDateCryptoPrice = getMaxDatePrice(file, date);
+                CryptoPrice minDateCryptoPrice = getMinDayPrice(file, date);
+                CryptoNormalizedRange cryptoNormalizedRange = getCryptoNormalizedRange(maxDateCryptoPrice.getPriceValue(),
+                        minDateCryptoPrice.getPriceValue(), maxDateCryptoPrice.getCurrencySymbol());
+                allCryptos.add(cryptoNormalizedRange);
+            } catch (NoSuchElementException exception) {
+                continue;
+            }
+        }
+        return allCryptos.stream().max(Comparator.comparing(CryptoNormalizedRange::getNormalizedRange)).orElseThrow();
+    }
+
+    private CryptoNormalizedRange getCryptoNormalizedRange(BigDecimal maxMonthPrice, BigDecimal minMonthPrice,
+                                                           String symbol) {
         BigDecimal normalizedRange = maxMonthPrice.subtract(minMonthPrice).divide(minMonthPrice, RoundingMode.CEILING);
         CryptoNormalizedRange cryptoNormalizedRange = new CryptoNormalizedRange();
-        cryptoNormalizedRange.setSymbol(maxMonthCryptoPrice.getCurrencySymbol());
+        cryptoNormalizedRange.setSymbol(symbol);
         cryptoNormalizedRange.setNormalizedRange(normalizedRange);
         return cryptoNormalizedRange;
     }
 
-    private List<CryptoPrice> getAllPricesForMonth(File file) throws IOException {
+    private List<CryptoPrice> getAllPrices(File file) throws IOException {
         return new CsvToBeanBuilder<CryptoPrice>(new FileReader(file))
                 .withType(CryptoPrice.class)
                 .build()
@@ -95,12 +120,26 @@ public class PriceService {
                         file.getName().matches(FILE_NAME_REGEXP));
     }
 
-    private CryptoPrice getMinMonthPrice(File file) throws IOException {
-        return getAllPricesForMonth(file).stream().min(Comparator.comparing(CryptoPrice::getPriceValue)).orElseThrow();
+    private CryptoPrice getMinPrice(File file) throws IOException {
+        return getAllPrices(file).stream().min(Comparator.comparing(CryptoPrice::getPriceValue)).orElseThrow();
     }
 
-    private CryptoPrice getMaxMonthPrice(File file) throws IOException {
-        return getAllPricesForMonth(file).stream().max(Comparator.comparing(CryptoPrice::getPriceValue)).orElseThrow();
+    private CryptoPrice getMaxPrice(File file) throws IOException {
+        return getAllPrices(file).stream().max(Comparator.comparing(CryptoPrice::getPriceValue)).orElseThrow();
+    }
+
+    private CryptoPrice getMinDayPrice(File file, LocalDate date) throws IOException {
+        return getAllPrices(file).stream()
+                .filter(cryptoPrice -> cryptoPrice.getPriceTime().toLocalDate().isEqual(date))
+                .min(Comparator.comparing(CryptoPrice::getPriceValue))
+                .orElseThrow();
+    }
+
+    private CryptoPrice getMaxDatePrice(File file, LocalDate date) throws IOException {
+        return getAllPrices(file).stream()
+                .filter(cryptoPrice -> cryptoPrice.getPriceTime().toLocalDate().isEqual(date))
+                .max(Comparator.comparing(CryptoPrice::getPriceValue))
+                .orElseThrow();
     }
 
     private File getFileFromSymbol(String symbol) throws IOException {
